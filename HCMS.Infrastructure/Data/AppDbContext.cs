e71f073235ba9fd8f035edb45e4be4132ef94520
MarioTonchev <mariotonchev500@gmail.com>
2025-07-14 23:17:21 +0300

    adding app db context

---
+++ HCMS.Infrastructure/Data/AppDbContext.cs
@@ -0,0 +1,21 @@
+﻿using HCMS.Infrastructure.Entities;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace HCMS.Infrastructure.Data
+{
+    public class AppDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Employee> Employees { get; set; }
+        public DbSet<Department> Departments { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
