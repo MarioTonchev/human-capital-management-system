ce9e9ea373f6db9eb13dd113e45361b0ad1ec81f
MarioTonchev <mariotonchev500@gmail.com>
2025-07-14 23:16:43 +0300

    adding db department entity

---
+++ HCMS.Infrastructure/Entities/Department.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HCMS.Infrastructure.Entities
+{
+    public class Department
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required, MaxLength(100)]
+        public string Name { get; set; } = default!;
+
+        public ICollection<Employee>? Employees { get; set; }
+    }
+}
