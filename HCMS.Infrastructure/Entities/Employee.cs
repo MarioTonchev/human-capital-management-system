cb032893941c10da35a468fbac50faa1c35b3337
MarioTonchev <mariotonchev500@gmail.com>
2025-07-14 23:16:53 +0300

    adding db employee entity

---
+++ HCMS.Infrastructure/Entities/Employee.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace HCMS.Infrastructure.Entities
+{
+    public class Employee
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required, MaxLength(50)]
+        public string FirstName { get; set; } = default!;
+
+        [Required, MaxLength(50)]
+        public string LastName { get; set; } = default!;
+
+        [Required, EmailAddress, MaxLength(100)]
+        public string Email { get; set; } = default!;
+
+        [Required, MaxLength(100)]
+        public string JobTitle { get; set; } = default!; //maybe make it an enum
+
+        [Required, Column(TypeName = "decimal(18,2)")]
+        public decimal Salary { get; set; }
+
+        public int? DepartmentId { get; set; }
+
+        [ForeignKey(nameof(DepartmentId))]
+        public Department? Department { get; set; }
+    }
+}
