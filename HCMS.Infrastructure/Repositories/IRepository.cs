ff66c27a71a0313708a71e13f20adede7b8080cd
MarioTonchev <mariotonchev500@gmail.com>
2025-07-15 03:08:25 +0300

    adding repository

---
+++ HCMS.Infrastructure/Repositories/IRepository.cs
@@ -0,0 +1,21 @@
+﻿using System.Linq.Expressions;
+
+namespace HCMS.Infrastructure.Repositories
+{
+    public interface IRepository
+    {
+        IQueryable<T> All<T>() where T : class;
+
+        IQueryable<T> AllAsReadOnly<T>() where T : class;
+
+        Task<T?> GetByIdAsync<T>(object id) where T : class;
+
+        Task<bool> ExistsAsync<T>(Expression<Func<T, bool>> predicate) where T : class;
+
+        Task AddAsync<T>(T entity) where T : class;
+
+        void Delete<T>(T entity) where T : class;
+
+        Task<int> SaveChangesAsync();
+    }
+}
---
+++ HCMS.Infrastructure/Repositories/Repository.cs
@@ -0,0 +1,56 @@
+﻿using HCMS.Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
+
+namespace HCMS.Infrastructure.Repositories
+{
+    public class Repository : IRepository
+    {
+        private readonly AppDbContext context;
+
+        public Repository(AppDbContext context)
+        {
+            this.context = context;
+        }
+
+        public IQueryable<T> All<T>() where T : class
+        {
+            return context.Set<T>();
+        }
+
+        public IQueryable<T> AllAsReadOnly<T>() where T : class
+        {
+            return context.Set<T>().AsNoTracking();
+        }
+
+        public async Task<T?> GetByIdAsync<T>(object id) where T : class
+        {
+            return await context.Set<T>().FindAsync(id);
+        }
+
+        public async Task<bool> ExistsAsync<T>(Expression<Func<T, bool>> predicate) where T : class
+        {
+            return await context.Set<T>().AnyAsync(predicate);
+        }
+
+        public async Task AddAsync<T>(T entity) where T : class
+        {
+            await context.Set<T>().AddAsync(entity);
+        }
+
+        public void Delete<T>(T entity) where T : class
+        {
+            if (entity == null)
+            {
+                throw new ArgumentNullException(nameof(entity));
+            }
+
+            context.Set<T>().Remove(entity);
+        }
+
+        public async Task<int> SaveChangesAsync()
+        {
+            return await context.SaveChangesAsync();
+        }
+    }
+}
