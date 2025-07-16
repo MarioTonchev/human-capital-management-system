c29e7e8d6188262136f481d386d5baccfcae99a7
MarioTonchev <mariotonchev500@gmail.com>
2025-07-16 17:33:04 +0300

    fixing spacing

--- HCMS.Core/DTOs/Employee/EmployeeDto.cs
+++ HCMS.Core/DTOs/Employee/EmployeeDto.cs
@@ -3,11 +3,17 @@
     public class EmployeeDto
     {
         public int Id { get; set; }
+
         public string FirstName { get; set; } = default!;
+
         public string LastName { get; set; } = default!;
+
         public string Email { get; set; } = default!;
+
         public string JobTitle { get; set; } = default!;
+
         public decimal Salary { get; set; }
+
         public int? DepartmentId { get; set; }
     }
 }
