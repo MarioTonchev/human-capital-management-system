84f8c84e54413f88686497658f638029d44ca8b9
MarioTonchev <mariotonchev500@gmail.com>
2025-07-17 00:22:31 +0300

    adding fk annotation for more clarification

--- HCMS.Infrastructure/Entities/ApplicationUser.cs
+++ HCMS.Infrastructure/Entities/ApplicationUser.cs
@@ -1,10 +1,13 @@
 ﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace HCMS.Infrastructure.Entities
 {
     public class ApplicationUser : IdentityUser
     {
         public int? EmployeeId { get; set; }
+            
+        [ForeignKey(nameof(EmployeeId))]
         public Employee? Employee { get; set; }
     }
 }
