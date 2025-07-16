f429c4e27bd35e61cb2a8e4723fc89bc6f54bafa
MarioTonchev <mariotonchev500@gmail.com>
2025-07-17 00:26:47 +0300

    adding jwt service which generates a jwt

---
+++ HCMS.BackendAPI/ApiServices/JWTService.cs
@@ -0,0 +1,48 @@
+﻿using HCMS.Core.Contracts;
+using HCMS.Infrastructure.Entities;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace HCMS.BackendAPI.ApiServices
+{
+    public class JWTService : IJWTService
+    {
+        private readonly IConfiguration configuration;
+
+        public JWTService(IConfiguration configuration)
+        {
+            this.configuration = configuration;
+        }
+
+        public string GenerateToken(ApplicationUser user, ICollection<string> roles)
+        {
+            var claims = new List<Claim>
+            {
+                new Claim(ClaimTypes.NameIdentifier, user.Id),
+                new Claim(ClaimTypes.Name, user.UserName),
+                new Claim(ClaimTypes.Email, user.Email)
+            };
+
+            foreach (var role in roles)
+            {
+                claims.Add(new Claim(ClaimTypes.Role, role));
+            }
+
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(configuration["Jwt:Key"]));
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+            var token = new JwtSecurityToken
+            (
+                issuer: configuration["Jwt:Issuer"],
+                audience: configuration["Jwt:Audience"],
+                claims: claims,
+                expires: DateTime.UtcNow.AddHours(1),
+                signingCredentials: creds
+            );
+
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+    }
+}
---
+++ HCMS.Core/Contracts/IJWTService.cs
@@ -0,0 +1,9 @@
+﻿using HCMS.Infrastructure.Entities;
+
+namespace HCMS.Core.Contracts
+{
+    public interface IJWTService
+    {
+        string GenerateToken(ApplicationUser user, ICollection<string> roles);
+    }
+}
