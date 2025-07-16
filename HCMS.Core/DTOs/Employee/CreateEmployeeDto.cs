c7b6cdcf5d43937579a5d8b9a919b36e4b6ff61a
MarioTonchev <mariotonchev500@gmail.com>
2025-07-16 16:10:27 +0300

    creating employee dtos

---
+++ HCMS.Core/DTOs/Employee/CreateEmployeeDto.cs
@@ -0,0 +1,12 @@
+﻿namespace HCMS.Core.DTOs.Employee
+{
+    public class CreateEmployeeDto
+    {
+        public string FirstName { get; set; } = default!;
+        public string LastName { get; set; } = default!;
+        public string Email { get; set; } = default!;
+        public string JobTitle { get; set; } = default!;
+        public decimal Salary { get; set; }
+        public int? DepartmentId { get; set; }
+    }
+}
---
+++ HCMS.Core/DTOs/Employee/EmployeeDto.cs
@@ -0,0 +1,13 @@
+﻿namespace HCMS.Core.DTOs.Employee
+{
+    public class EmployeeDto
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; } = default!;
+        public string LastName { get; set; } = default!;
+        public string Email { get; set; } = default!;
+        public string JobTitle { get; set; } = default!;
+        public decimal Salary { get; set; }
+        public int? DepartmentId { get; set; }
+    }
+}
