afaf9ea65228ee916cb93fd581ad60253349af15
MarioTonchev <mariotonchev500@gmail.com>
2025-07-16 16:09:55 +0300

    adding migrations for db seeding

---
+++ HCMS.Infrastructure/Migrations/20250715143745_SeedDatabase.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace HCMS.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class SeedDatabase : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Departments",
+                columns: new[] { "Id", "Name" },
+                values: new object[,]
+                {
+                    { 1, "IT" },
+                    { 2, "HR" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Employees",
+                columns: new[] { "Id", "DepartmentId", "Email", "FirstName", "JobTitle", "LastName", "Salary" },
+                values: new object[,]
+                {
+                    { 1, 1, "pesho@gmail.com", "Pesho", "Software Developer", "Peshov", 5000m },
+                    { 2, 1, "gosho@gmail.com", "Gosho", "AI Engineer", "Goshov", 6000m },
+                    { 3, 2, "koko@gmail.com", "Koko", "HR Employee", "Kokov", 5000m }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Employees",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Employees",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Employees",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Departments",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Departments",
+                keyColumn: "Id",
+                keyValue: 2);
+        }
+    }
+}
