69edb09949aba52dc34678a7f63e760c1dd8ee96
MarioTonchev <mariotonchev500@gmail.com>
2025-07-16 16:10:39 +0300

    adding service interfaces

---
+++ HCMS.Core/Contracts/IDepartmentService.cs
@@ -0,0 +1,13 @@
+﻿using HCMS.Core.DTOs.Department;
+
+namespace HCMS.Core.Contracts
+{
+    public interface IDepartmentService
+    {
+        Task<IEnumerable<DepartmentDto>> GetAllAsync();
+        Task<DepartmentDto?> GetByIdAsync(int id);
+        Task CreateAsync(CreateDepartmentDto dto);
+        Task UpdateAsync(int id, CreateDepartmentDto dto);
+        Task DeleteAsync(int id);
+    }
+}
---
+++ HCMS.Core/Contracts/IEmployeeService.cs
@@ -0,0 +1,15 @@
+﻿using HCMS.Core.DTOs.Employee;
+
+namespace HCMS.Core.Contracts
+{
+    public interface IEmployeeService
+    {
+        Task<IEnumerable<EmployeeDto>> GetAllAsync();
+        Task<EmployeeDto?> GetByIdAsync(int id);
+        Task CreateAsync(CreateEmployeeDto dto);
+        Task UpdateAsync(int id, CreateEmployeeDto dto);
+        Task DeleteAsync(int id);
+        Task<IEnumerable<EmployeeDto>> GetByDepartmentIdAsync(int departmentId);
+        Task<IEnumerable<EmployeeDto>> GetByDepartmentNameAsync(string departmentName);
+    }
+}
