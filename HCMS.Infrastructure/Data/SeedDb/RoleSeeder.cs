ee00ef62d7651c8e338cf1cda41ac89551be372c
MarioTonchev <mariotonchev500@gmail.com>
2025-07-16 16:10:03 +0300

    adding role seeder

---
+++ HCMS.Infrastructure/Data/SeedDb/RoleSeeder.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace HCMS.Infrastructure.Data.SeedDb
+{
+    public static class RoleSeeder
+    {
+        private static readonly string[] roles = ["Employee", "Manager", "HRAdmin"];
+
+        public static async Task SeedAsync(IServiceProvider serviceProvider)
+        {
+            var roleManager = serviceProvider.GetRequiredService<RoleManager<IdentityRole>>();
+
+            foreach (var role in roles)
+            {
+                if (!await roleManager.RoleExistsAsync(role))
+                {
+                    await roleManager.CreateAsync(new IdentityRole(role));
+                }
+            }
+
+        }
+    }
+}
