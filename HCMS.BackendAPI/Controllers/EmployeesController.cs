4f10b8abdd86353d6bc45b871cf479f08fd3431f
MarioTonchev <mariotonchev500@gmail.com>
2025-07-17 00:28:59 +0300

    removing create employee endpoint and modifying controller

--- HCMS.BackendAPI/Controllers/EmployeesController.cs
+++ HCMS.BackendAPI/Controllers/EmployeesController.cs
@@ -1,65 +1,124 @@
 ﻿using HCMS.Core.Contracts;
 using HCMS.Core.DTOs.Employee;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace HCMS.BackendAPI.Controllers
 {
+    [Authorize]
     [Route("api/[controller]")]
     [ApiController]
     public class EmployeesController : ControllerBase
     {
         private readonly IEmployeeService employeeService;
+        private readonly ICurrentUserService currentUserService;
 
-        public EmployeesController(IEmployeeService employeeService)
+        public EmployeesController(IEmployeeService employeeService, ICurrentUserService currentUserService)
         {
             this.employeeService = employeeService;
+            this.currentUserService = currentUserService;
         }
 
-        [HttpGet]
-        public async Task<IActionResult> GetAll()
+        [HttpGet("me")]
+        public async Task<IActionResult> GetMyProfile()
         {
+            var user = await currentUserService.GetCurrentUserAsync();
+
+            if (user == null)
+            {
+                return Unauthorized();
+            }
+
+            var employee = await employeeService.GetByIdAsync((int)user.EmployeeId);
+
+            return employee == null ? NotFound() : Ok(employee);
+        }
+
+        [Authorize(Roles = "HRAdmin,Manager")]
+        [HttpGet]
+        public async Task<IActionResult> GetAllEmployees()
+        {
+            var currUser = await currentUserService.GetCurrentUserAsync();
             var employees = await employeeService.GetAllAsync();
 
-            return Ok(employees);
+            if (User.IsInRole("HRAdmin"))
+            {
+                return Ok(employees);
+            }
+
+            //Here if the current user is a manager he will get only the employees from his own department
+            if (User.IsInRole("Manager"))
+            {
+                var employeesByDepartment = await employeeService.GetByDepartmentIdAsync((int)currUser.Employee.DepartmentId);
+                return Ok(employeesByDepartment);
+            }
+
+            return Forbid();
         }
 
+        [Authorize(Roles = "HRAdmin,Manager")]
         [HttpGet("{id}")]
-        public async Task<IActionResult> GetById(int id)
+        public async Task<IActionResult> GetEmployeeById(int id)
         {
-            var employee = await employeeService.GetByIdAsync(id);
+            var currUser = await currentUserService.GetCurrentUserAsync();
+            var employeeToFind = await employeeService.GetByIdAsync(id);
 
-            if (employee == null)
+            if (employeeToFind == null)
             {
                 return NotFound();
             }
 
-            return Ok(employee);
-        }
-
-        [HttpPost]
-        public async Task<IActionResult> Create([FromBody] CreateEmployeeDto dto)
-        {
-            if (!ModelState.IsValid)
+            if (User.IsInRole("HRAdmin"))
             {
-                return BadRequest(ModelState);
+                return Ok(employeeToFind);
             }
 
-            var created = await employeeService.CreateAsync(dto);
+            if (User.IsInRole("Manager"))
+            {
+                if (employeeToFind.DepartmentId == currUser.Employee.DepartmentId)
+                {
+                    return Ok(employeeToFind);
+                }
+            }
 
-            return CreatedAtAction(nameof(GetById), new {id = created.Id}, created);
+            return Forbid();
         }
 
+        //todo: rename this dto for clarification
+        [Authorize(Roles = "HRAdmin,Manager")]
         [HttpPut("{id}")]
-        public async Task<IActionResult> Update(int id, [FromBody] CreateEmployeeDto dto)
+        public async Task<IActionResult> UpdateEmployee(int id, [FromBody] CreateEmployeeDto dto)
         {
             if (!ModelState.IsValid)
             {
                 return BadRequest(ModelState);
             }
 
-            var updated = await employeeService.UpdateAsync(id, dto);
+            var currUser = await currentUserService.GetCurrentUserAsync();
 
-            if (!updated)
+            if (currUser == null)
+            {
+                return Unauthorized();
+            }
+
+            bool isUpdated = false;
+
+            if (User.IsInRole("HRAdmin"))
+            {
+                isUpdated = await employeeService.UpdateAsync(id, dto);
+            }
+
+            if (User.IsInRole("Manager"))
+            {
+                var userToUpdate = await employeeService.GetByIdAsync(id);
+
+                if (userToUpdate.DepartmentId == currUser.Employee.DepartmentId)
+                {
+                    isUpdated = await employeeService.UpdateAsync(id, dto);
+                }
+            }
+
+            if (!isUpdated)
             {
                 return NotFound();
             }
@@ -67,9 +126,11 @@ namespace HCMS.BackendAPI.Controllers
             return NoContent();
         }
 
+        [Authorize(Roles = "HRAdmin")]
         [HttpDelete("{id}")]
-        public async Task<IActionResult> Delete(int id)
+        public async Task<IActionResult> DeleteEmployee(int id)
         {
+            //todo: also delete corresponding application user
             var deleted = await employeeService.DeleteAsync(id);
 
             if (!deleted)
