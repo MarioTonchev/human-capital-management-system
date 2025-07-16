5e1bdfe8842babbe5ec748945534807c952f85fc
MarioTonchev <mariotonchev500@gmail.com>
2025-07-17 00:29:06 +0300

    modifying controller

--- HCMS.BackendAPI/Controllers/DepartmentsController.cs
+++ HCMS.BackendAPI/Controllers/DepartmentsController.cs
@@ -1,20 +1,25 @@
 ﻿using HCMS.Core.Contracts;
 using HCMS.Core.DTOs.Department;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace HCMS.BackendAPI.Controllers
 {
+    [Authorize(Roles = "HRAdmin,Manager")]
     [Route("api/[controller]")]
     [ApiController]
     public class DepartmentsController : ControllerBase
     {
         private readonly IDepartmentService departmentService;
+        private readonly ICurrentUserService currentUserService;
 
-        public DepartmentsController(IDepartmentService departmentService)
+        public DepartmentsController(IDepartmentService departmentService, ICurrentUserService currentUserService)
         {
             this.departmentService = departmentService;
+            this.currentUserService = currentUserService;
         }
 
+        [Authorize(Roles = "HRAdmin")]
         [HttpGet]
         public async Task<IActionResult> GetAll()
         {
@@ -23,9 +28,11 @@ namespace HCMS.BackendAPI.Controllers
             return Ok(departments);
         }
 
+        [Authorize(Roles = "HRAdmin,Manager")]
         [HttpGet("{id}")]
         public async Task<IActionResult> GetById(int id)
         {
+            var currUser = await currentUserService.GetCurrentUserAsync();
             var dept = await departmentService.GetByIdAsync(id);
 
             if (dept == null)
@@ -33,9 +40,23 @@ namespace HCMS.BackendAPI.Controllers
                 return NotFound();
             }
 
-            return Ok(dept);
+            if (User.IsInRole("HRAdmin"))
+            {
+                return Ok(dept);
+            }
+
+            if (User.IsInRole("Manager"))
+            {
+                if (dept.Id == currUser.Employee.DepartmentId)
+                {
+                    return Ok(dept);
+                }
+            }
+
+            return Forbid();
         }
 
+        [Authorize(Roles = "HRAdmin")]
         [HttpPost]
         public async Task<IActionResult> Create([FromBody] CreateDepartmentDto dto)
         {
@@ -49,6 +70,7 @@ namespace HCMS.BackendAPI.Controllers
             return CreatedAtAction(nameof(GetById), new { id = created.Id }, created);
         }
 
+        [Authorize(Roles = "HRAdmin")]
         [HttpPut("{id}")]
         public async Task<IActionResult> Update(int id, [FromBody] CreateDepartmentDto dto)
         {
@@ -67,6 +89,7 @@ namespace HCMS.BackendAPI.Controllers
             return NoContent();
         }
 
+        [Authorize(Roles = "HRAdmin")]
         [HttpDelete("{id}")]
         public async Task<IActionResult> Delete(int id)
         {
