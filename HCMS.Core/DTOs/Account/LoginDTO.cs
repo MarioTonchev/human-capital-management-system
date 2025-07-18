0617195903a50ffc877fb8a2e7a1aae9942bf96d
MarioTonchev <mariotonchev500@gmail.com>
2025-07-18 19:14:27 +0300

    adding property constraints for validation

--- HCMS.Core/DTOs/Account/LoginDTO.cs
+++ HCMS.Core/DTOs/Account/LoginDTO.cs
@@ -1,9 +1,16 @@
-﻿namespace HCMS.Core.DTOs.Account
+﻿using System.ComponentModel.DataAnnotations;
+using static HCMS.Infrastructure.Constants.EntityConstants.ApplicationUserConstants;
+
+namespace HCMS.Core.DTOs.Account
 {
     public class LoginDTO
     {
+        [Required]
+        [MaxLength(UsernameMaxLength)]
         public string Username { get; set; } = default!;
 
+        [Required]
+        [MaxLength(PasswordMaxLength)]
         public string Password { get; set; } = default!;
     }
 }
--- HCMS.Core/DTOs/Account/RegisterUserDTO.cs
+++ HCMS.Core/DTOs/Account/RegisterUserDTO.cs
@@ -1,26 +1,43 @@
-﻿namespace HCMS.Core.DTOs.Account
+﻿using System.ComponentModel.DataAnnotations;
+using static HCMS.Infrastructure.Constants.EntityConstants.EmployeeConstants;
+using static HCMS.Infrastructure.Constants.EntityConstants.ApplicationUserConstants;
+
+namespace HCMS.Core.DTOs.Account
 {
-    //TODO: add constraints
     public class RegisterUserDTO
     {
-        // User related properties
+        // User identity related properties
+        [Required]
+        [MaxLength(UsernameMaxLength)]
         public string Username { get; set; } = default!;
 
-        public string Email { get; set; } = default!;
-
+        [Required]
+        [MaxLength(PasswordMaxLength)]
         public string Password { get; set; } = default!;
 
+        [Required]
         public string Role { get; set; } = default!; // Employee, Manager, HRAdmin
 
-        public int EmployeeId { get; set; }
-
         // Employee (domain-specific) related properties
+        [Required(ErrorMessage = $"{nameof(FirstName)} is required")]
+        [MaxLength(EmployeeFirstNameMaxLength)]
         public string FirstName { get; set; } = default!;
 
+        [Required(ErrorMessage = $"{nameof(LastName)} is required")]
+        [MaxLength(EmployeeLastNameMaxLength)]
         public string LastName { get; set; } = default!;
 
+        [Required(ErrorMessage = $"{nameof(Email)} is required")]
+        [EmailAddress(ErrorMessage = "Invalid email format")]
+        [MaxLength(EmployeeEmailMaxLength)]
+        public string Email { get; set; } = default!;
+
+        [Required(ErrorMessage = $"{nameof(JobTitle)} is required")]
+        [MaxLength(EmployeeJobTitleMaxLength)]
         public string JobTitle { get; set; } = default!;
 
+        [Required(ErrorMessage = $"{nameof(Salary)} is required")]
+        [Range(0, double.MaxValue, ErrorMessage = "Salary must be a non-negative number")]
         public decimal Salary { get; set; }
 
         public int? DepartmentId { get; set; }
