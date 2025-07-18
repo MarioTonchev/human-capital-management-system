7c34ee1ff0f289e82d6b0ad77b4ddaae30b98889
MarioTonchev <mariotonchev500@gmail.com>
2025-07-18 19:15:11 +0300

    adding account ui service which communicates with backend through http

---
+++ HCMS.Web/Contracts/IAccountUIService.cs
@@ -0,0 +1,12 @@
+﻿using HCMS.Core.DTOs.Account;
+
+namespace HCMS.UI.Contracts
+{
+    public interface IAccountUIService
+    {
+        Task<bool> LoginAsync(LoginDTO loginDto);
+        Task<bool> RegisterAsync(RegisterUserDTO registerDto);
+        Task LogoutAsync();
+        Task<bool> DeleteUserAsync(int id);
+    }
+}
---
+++ HCMS.Web/UIServices/AccountUIService.cs
@@ -0,0 +1,71 @@
+﻿using HCMS.Core.DTOs.Account;
+using HCMS.UI.Contracts;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Text.Json;
+
+namespace HCMS.UI.UIServices
+{
+    public class AccountUIService : IAccountUIService
+    {
+        private readonly HttpClient client;
+        private readonly IHttpContextAccessor httpContextAccessor;
+
+        public AccountUIService(IHttpClientFactory factory, IHttpContextAccessor httpContextAccessor)
+        {
+            client = factory.CreateClient("BackendApi");
+            this.httpContextAccessor = httpContextAccessor;
+        }
+
+        public async Task<bool> LoginAsync(LoginDTO dto)
+        {
+            var response = await client.PostAsJsonAsync("account/login", dto);
+            Console.WriteLine(client.BaseAddress);
+            if (!response.IsSuccessStatusCode)
+            {
+                return false;
+            }
+
+            var jwt = await response.Content.ReadAsStringAsync();
+
+            httpContextAccessor.HttpContext?.Session.SetString("JWT", jwt);
+
+            return true;
+        }
+
+        public Task LogoutAsync()
+        {
+            httpContextAccessor.HttpContext?.Session.Remove("JWT");
+
+            return Task.CompletedTask;
+        }
+
+        public async Task<bool> RegisterAsync(RegisterUserDTO dto)
+        {
+            AddAuthHeaderIfAvailable();
+
+            var response = await client.PostAsJsonAsync("account/register-user", dto);
+
+            return response.IsSuccessStatusCode;
+        }
+
+        public async Task<bool> DeleteUserAsync(int id)
+        {
+            AddAuthHeaderIfAvailable();
+
+            var response = await client.DeleteAsync($"account/{id}");
+
+            return response.IsSuccessStatusCode;
+        }
+
+        private void AddAuthHeaderIfAvailable()
+        {
+            var token = httpContextAccessor.HttpContext?.Session.GetString("JWT");
+
+            if (!string.IsNullOrWhiteSpace(token))
+            {
+                client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
+            }
+        }
+    }
+}
