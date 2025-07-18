06e5d0fe2ff924e4a9bf0c31cdaaa887055fe539
MarioTonchev <mariotonchev500@gmail.com>
2025-07-18 19:16:16 +0300

    registering services and adding session

--- HCMS.Web/Program.cs
+++ HCMS.Web/Program.cs
@@ -1,21 +1,44 @@
+using HCMS.UI.Contracts;
+using HCMS.UI.UIServices;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddHttpClient("BackendApi", client =>
+{
+    client.BaseAddress = new Uri("http://localhost:5041/api/");
+});
+
+builder.Services.AddHttpContextAccessor();
+
+builder.Services.AddScoped<IAccountUIService, AccountUIService>();
+builder.Services.AddScoped<IEmployeeUIService, EmployeeUIService>();
+builder.Services.AddScoped<IDepartmentUIService, DepartmentUIService>();
+
+builder.Services.AddDistributedMemoryCache(); 
+
+builder.Services.AddSession(options =>
+{
+    options.IdleTimeout = TimeSpan.FromMinutes(30); 
+    options.Cookie.HttpOnly = true;                 
+    options.Cookie.IsEssential = true;              
+});
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
 app.UseHttpsRedirection();
 app.UseRouting();
 
+app.UseSession();
+
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapStaticAssets();
