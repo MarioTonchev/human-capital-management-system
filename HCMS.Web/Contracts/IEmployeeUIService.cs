3c3dc01af196730e11c02188a33baf45c598a15a
MarioTonchev <mariotonchev500@gmail.com>
2025-07-20 04:12:17 +0300

    adding employee ui service interface

---
+++ HCMS.Web/Contracts/IEmployeeUIService.cs
@@ -0,0 +1,12 @@
+﻿using HCMS.Core.DTOs.Employee;
+
+namespace HCMS.UI.Contracts
+{
+    public interface IEmployeeUIService
+    {
+        Task<EmployeeDto> GetMyProfileAsync();
+        Task<List<EmployeeDto>> GetAllEmployeesAsync();
+        Task<EmployeeDto?> GetEmployeeByIdAsync(int id);
+        Task<bool> UpdateEmployeeAsync(int id, UpdateEmployeeDto dto);
+    }
+}
