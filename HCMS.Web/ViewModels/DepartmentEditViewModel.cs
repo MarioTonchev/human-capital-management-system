14b21758a50246221958f341428c54af2e43c770
MarioTonchev <mariotonchev500@gmail.com>
2025-07-20 04:12:57 +0300

    adding dep view model for editing

---
+++ HCMS.Web/ViewModels/DepartmentEditViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+using static HCMS.Infrastructure.Constants.EntityConstants.DepartmentConstants;
+
+namespace HCMS.UI.ViewModels
+{
+    public class DepartmentEditViewModel
+    {
+        [Required]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = $"{nameof(Name)} is required")]
+        [MaxLength(DepartmentNameMaxLength)]
+        public string Name{ get; set; } = default!;
+    }
+}
