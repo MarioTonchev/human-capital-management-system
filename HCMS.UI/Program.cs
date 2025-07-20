7e96a52b70d1447f1693328e0aa37313639c3191
MarioTonchev <mariotonchev500@gmail.com>
2025-07-20 19:18:17 +0300

    cleaning program.cs

--- HCMS.UI/Program.cs
+++ HCMS.UI/Program.cs
@@ -1,65 +1,16 @@
-using HCMS.UI.Contracts;
-using HCMS.UI.UIServices;
-using Microsoft.AspNetCore.Authentication.JwtBearer;
-using Microsoft.IdentityModel.Tokens;
-using System.Text;
+using HCMS.UI.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
 
+// Adding services to the container.
+
 builder.Services.AddControllersWithViews();
+builder.Services.ConfigureHttpClient();
+builder.Services.ConfigureSession();
+builder.Services.ConfigureAuthentication(builder.Configuration);
+builder.Services.ConfigureUIServices();
 
-builder.Services.AddHttpClient("BackendApi", client =>
-{
-    client.BaseAddress = new Uri("https://localhost:7248/api/");
-});
-
-builder.Services.AddHttpContextAccessor();
-
-builder.Services.AddScoped<IAccountUIService, AccountUIService>();
-builder.Services.AddScoped<IEmployeeUIService, EmployeeUIService>();
-builder.Services.AddScoped<IDepartmentUIService, DepartmentUIService>();
-
-builder.Services.AddAuthentication(options =>
-{
-    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
-    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
-})
-.AddJwtBearer(options =>
-{
-    options.TokenValidationParameters = new TokenValidationParameters
-    {
-        ValidateIssuer = false,
-        ValidateAudience = false,
-        ValidateLifetime = true,
-        ValidateIssuerSigningKey = true,
-        IssuerSigningKey = new SymmetricSecurityKey(
-            Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"]))
-    };
-
-    options.Events = new JwtBearerEvents
-    {
-        OnMessageReceived = context =>
-        {
-            var jwt = context.HttpContext.Session.GetString("JWT");
-
-            if (!string.IsNullOrEmpty(jwt))
-            {
-                context.Token = jwt;
-            }
-
-            return Task.CompletedTask;
-        }
-    };
-});
-
-builder.Services.AddDistributedMemoryCache(); 
-
-builder.Services.AddSession(options =>
-{
-    options.IdleTimeout = TimeSpan.FromMinutes(30); 
-    options.Cookie.HttpOnly = true;                 
-    options.Cookie.IsEssential = true;              
-});
+// Building the application.
 
 var app = builder.Build();
 
@@ -69,20 +20,17 @@ if (!app.Environment.IsDevelopment())
     app.UseHsts();
 }
 
-app.UseSession();
+// Configuring middleware pipeline.
 
+app.UseSession();
 app.UseHttpsRedirection();
 app.UseRouting();
-
 app.UseAuthentication();
 app.UseAuthorization();
-
 app.MapStaticAssets();
-
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}")
     .WithStaticAssets();
-
 
 app.Run();
