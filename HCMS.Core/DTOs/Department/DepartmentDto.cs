0f22920766528086d57afebeef36723cfa122098
MarioTonchev <mariotonchev500@gmail.com>
2025-07-20 20:59:50 +0300

    adding employees in department dto

--- HCMS.Core/DTOs/Department/DepartmentDto.cs
+++ HCMS.Core/DTOs/Department/DepartmentDto.cs
@@ -1,9 +1,13 @@
-﻿namespace HCMS.Core.DTOs.Department
+﻿using HCMS.Core.DTOs.Employee;
+
+namespace HCMS.Core.DTOs.Department
 {
     public class DepartmentDto
     {
         public int Id { get; set; }
 
         public string Name { get; set; } = default!;
+
+        public List<EmployeeDto>? Employees { get; set; }
     }
 }
