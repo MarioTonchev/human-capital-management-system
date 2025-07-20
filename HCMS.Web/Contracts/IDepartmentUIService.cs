7eb1e29dd6149fb31bba71d3c979156e161e358d
MarioTonchev <mariotonchev500@gmail.com>
2025-07-20 04:11:53 +0300

    adding dep ui service interface

---
+++ HCMS.Web/Contracts/IDepartmentUIService.cs
@@ -0,0 +1,13 @@
+﻿using HCMS.Core.DTOs.Department;
+
+namespace HCMS.UI.Contracts
+{
+    public interface IDepartmentUIService
+    {
+        Task<List<DepartmentDto>> GetAllDepartmentsAsync();
+        Task<DepartmentDto> GetDepartmentByIdAsync(int id);
+        Task<bool> CreateDepartmentAsync(CreateDepartmentDto dto);
+        Task<bool> UpdateDepartmentAsync(int id, UpdateDepartmentDto dto);
+        Task<bool> DeleteDepartmentAsync(int id);
+    }
+}
